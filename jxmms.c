dcd38ce69bcb1d256aa7f86841e3b32d3825bdaa
Karl <karl@karl-desktop.(none)>
2009-11-04 18:23:38 -0500

    Added /xmms to display song - convert to X-Chat command

--- jxmms.c
+++ jxmms.c
@@ -21,6 +21,12 @@ static xchat_plugin *ph;   /* plugin handle */
 #define PDESC "XMMS Now Playing Script"
 #define PVERSION "0.1"
 
+#define IS_XMMS_RUNNING() \
+	if (!xmms_remote_is_running(session)) { \
+		xchat_print(ph, "Xmms is not running, exiting.."); \
+		return XCHAT_EAT_ALL; \
+	}  
+
 int color_b1 = 0;
 int color_b2 = 0;
 int color_info = 4;
@@ -33,21 +39,56 @@ int color_bar3 = 0;
 
 
 int play_pause(char *word[], char *word_eol[], void *userdata) {
+	IS_XMMS_RUNNING()
 	xmms_remote_play_pause(session);
 	return XCHAT_EAT_ALL;
 }
 
 int xmms_next(char *word[], char *word_eol[], void *userdata) {
+	IS_XMMS_RUNNING()
 	xmms_remote_playlist_next(session);
 	return XCHAT_EAT_ALL;
 }
 
 int xmms_prev(char *word[], char *word_eol[], void *userdata) {
+	IS_XMMS_RUNNING()
 	xmms_remote_playlist_prev(session);
 	return XCHAT_EAT_ALL;
 }
 
+int get_song(char *word[], char *word_eol[], void *userdata) {
+	IS_XMMS_RUNNING()
 
+	int rate, nch, freq, vol;
+	struct stat;
+	int session=0;
+	char ratestr[200];
+	char song_filename[300];
+	char song_name[300];
+	char song_time[100];
+	char freqhz[100];
+	int song_position = xmms_remote_get_playlist_pos(session);
+	int playlist_length = xmms_remote_get_playlist_length(session);
+
+	/* grab the bitrate, freq and number of channels */
+	xmms_remote_get_info(session, &rate, &freq, &nch);
+	
+	vol = xmms_remote_get_main_volume(session);
+
+	convert_rate(rate, ratestr);
+	convert_time(session, song_position, song_time);
+
+	sprintf(song_filename, "%s", xmms_remote_get_playlist_file(session, song_position));
+	sprintf(song_name, "%s",xmms_remote_get_playlist_title(session, xmms_remote_get_playlist_pos(session))); 	
+	sprintf(freqhz, "%02d.%.01d", freq / 1000, freq % 1000);
+
+	char msg[1024];
+	sprintf(msg, "ME playing: %s (%s) (%s kHz) %s (%i/%i)", song_name, ratestr,freqhz,song_time,song_position+1,playlist_length);
+
+	xchat_command(ph,msg);		
+
+	return XCHAT_EAT_ALL;
+}
 
 int xchat_plugin_init(xchat_plugin *plugin_handle,
                       char **plugin_name,
@@ -67,10 +108,10 @@ int xchat_plugin_init(xchat_plugin *plugin_handle,
 	xchat_hook_command(ph, "xmms-next", XCHAT_PRI_NORM, xmms_next, "Usage: xmms-next", 0);
 	xchat_hook_command(ph, "xmms-prev", XCHAT_PRI_NORM, xmms_prev, "Usage: xmms-prev", 0);
 	
-	// xchat_hook_print(ph, "Join", XCHAT_PRI_NORM, join_cb, 0);
 	char msg[200];
 	sprintf(msg, "\003%d[\003%d\002Jaguar-XMMS Now Playing Script v.%s\017\003%d]\003%d by \003%d\002 Jaguar\017 \003%dloaded successfully!", color_b2, color_title, PVERSION, color_b2, color_text, color_info, color_text);
 	xchat_print(ph, msg);
+
 	return 1;
 }
 
@@ -91,8 +132,6 @@ int main (void) {
 		return 0;
 	}
 	
-	get_song();
-	
 	return 1;
 }
 
@@ -101,44 +140,6 @@ int get_version(session) {
 	version = xmms_remote_get_version(session);
 	return version;
 }
-int get_song(void) {
-
-	int rate, nch, freq, vol;
-	struct stat;
-	int session=0;
-	char ratestr[200];
-	char song_filename[300];
-	char song_name[300];
-	char song_time[100];
-	char freqhz[100];
-	int song_position = xmms_remote_get_playlist_pos(session);
-	int playlist_length = xmms_remote_get_playlist_length(session);
-
-	/* stat(xmms_remote_get_playlist_file(session, song_position), &filestat); */
-	/* grab the bitrate, freq and number of channels */
-	xmms_remote_get_info(session, &rate, &freq, &nch);
-	
-	/* Get the volume. if it's 100%, change it to 110% for l33tness :) */
-	vol = xmms_remote_get_main_volume(session);
-	/* if (vol == 100) { vol = 110; } */
-	convert_rate(rate, ratestr);
-	convert_time(session, song_position, song_time);
-	sprintf(song_filename, "%s", xmms_remote_get_playlist_file(session, song_position));
-	sprintf(song_name, "%s",xmms_remote_get_playlist_title(session, xmms_remote_get_playlist_pos(session))); 	
-	sprintf(freqhz, "%02d.%.01d", freq / 1000, freq % 1000);
-
-/*
-	printf("song: %s Volume: %i rate: %s freq: %s kHz songtime: %s file loc: %s", song_name, vol, ratestr, freqhz, song_time, song_filename);
-	printf(" playlist: [%i/%i]\n", song_position+1, playlist_length);
-*/
-	printf("playing: %s (%s) (%s kHz) %s (%i/%i)\n", song_name, ratestr,freqhz,song_time,song_position+1,playlist_length);
-		
-	/* ugly as fuck, i know :) */
-	/* xchat_commandf(ph,now_playing_string, xmms_remote_get_playlist_title(session, xmms_remote_get_playlist_pos(session))); */
-
-	return 0;
-}
-
 char* convert_rate(int rate, char* ratestr) {
 	rate = rate / 1000;
 	sprintf(ratestr, "%i Kbps", rate);
